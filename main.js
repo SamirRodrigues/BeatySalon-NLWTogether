c393d4d3991587c2dfa3ee04a428496c1afe11e3
SamirRodrigues <emaildesamir@gmail.com>
2021-07-30 02:28:01 -0300

    Full Application

---
+++ main.js
@@ -0,0 +1,115 @@
+/** ATIVA E DESATIVA BARRA DE MENU LATERAL */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+/** TROCA DE ATIVADO PARA DESATIVADO E VICE VERSA */
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/** SOMENTE DESATIVA */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*ATIVAR EFEITO DE SOMBRA NO HEADER QUANDO DER SCROLL*/
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/** TESTIMONAIS SWIPER*/
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mausewheel: true,
+  keyboard: true,
+  loop: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/** SCROLL REVEAL: MOSTRAR ELEMENTOS COM ANIMAÇÃO QUANDO DER SCROLL NA PÁGINA*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image,  #home .text,
+  #about .image,  #about .text,
+  #services header, #services .card,
+  #testimonials header, 
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+
+/**BUTTON BACK TO TOP */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/** DESTAQUE DO MENU ATIVO */
+
+const sections = document.querySelectorAll('main section[id]')
+function menuHighlight() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  menuHighlight()
+})
